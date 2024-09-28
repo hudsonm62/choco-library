e5d8da5ab81afff62097838c6d673deb5d889744
Hudson <hudson@ziroau.com>
2024-09-28 20:30:28 +1000

    Add `drawio`
    i want the msi version

---
+++ packages/drawio/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = 'drawio'
+  fileType      = 'MSI'
+  url64bit      = 'https://github.com/jgraph/drawio-desktop/releases/download/v24.7.8/draw.io-24.7.8.msi'
+  checksum64    = 'c10a2b4f5a1438549db3bf387f8a20e73aa55eacb78ddf9945dc1e073bfea442'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ packages/drawio/update.ps1
@@ -0,0 +1,32 @@
+Import-Module Chocolatey-AU
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+        }
+        "drawio.nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $headers = @{
+      "Accept" = "application/vnd.github+json"
+      "X-GitHub-Api-Version" = "2022-11-28"
+    }
+    $url = "https://api.github.com/repos/jgraph/drawio-desktop/releases/latest"
+    $latestRelease = Invoke-RestMethod -Uri $url -Headers $headers -Method Get
+    $latestVersion = $latestRelease.name
+
+    return @{
+        Version      = $latestVersion
+        URL64        = ($latestRelease.assets | Where-Object {$_.name.EndsWith(".msi")}).browser_download_url
+        ReleaseNotes = $latestRelease.html_url
+    }
+}
+
+update -ChecksumFor 64
