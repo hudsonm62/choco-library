aeacca33c7cf17d8fcc4533a76ed173ceda801b0
hudsonm62 <hudsonm62@users.noreply.github.com>
2024-10-02 13:18:14 +0000

    Choco-AU : Applied Automatic Package Updates

--- packages/windirstat-next/tools/chocolateyInstall.ps1
+++ packages/windirstat-next/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop'
-$url = ''
-$checksum = ''
-$checksumType = ''
-$url64 = ''
-$checksum64 = ''
-$checksumType64 = ''
+﻿$ErrorActionPreference = 'Stop'
+$url = 'https://github.com/windirstat/windirstat-next/releases/download/Release_2_0_0_2024_09_19/WinDirStat-x86.msi'
+$checksum = 'e69c26dcf72a50a968a8b67ad7fc36833a9093d429da94c0d4fca94a9f8c86a5'
+$checksumType = 'sha256'
+$url64 = 'https://github.com/windirstat/windirstat-next/releases/download/Release_2_0_0_2024_09_19/WinDirStat-x64.msi'
+$checksum64 = 'd562f584bcd0a0ee902e513d69d15bfe43857baf5ec0efdaee84224ac6a99f0a'
+$checksumType64 = 'sha256'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
