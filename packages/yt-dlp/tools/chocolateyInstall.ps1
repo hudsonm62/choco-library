8969ba95f67678dc59e17ff5bc4162b07038d55f
hudsonm62 <hudsonm62@users.noreply.github.com>
2024-10-23 04:09:37 +0000

    Choco-AU : Applied Automatic Package Updates

--- packages/yt-dlp/tools/chocolateyInstall.ps1
+++ packages/yt-dlp/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://github.com/yt-dlp/yt-dlp/releases/download/2024.10.07/yt-dlp_x86.exe'
-$checksum32 = '06fd338f7e1c179b7d6ccd4212ab3f2f1861ea9c5f5cd9d7ef0c1254eaf69a27'
+$url = 'https://github.com/yt-dlp/yt-dlp/releases/download/2024.10.22/yt-dlp_x86.exe'
+$checksum32 = 'ba63c0a53d1f50d1ee1e2e5e87839c5a1321cbeaaf4196003b7aee46c591b364'
 $checksumType32 = 'sha256'
-$url64 = 'https://github.com/yt-dlp/yt-dlp/releases/download/2024.10.07/yt-dlp.exe'
-$checksum64 = 'fdf3856b6392e9b96637229294993dae2331f2565fcdfc8cccada74927dc31bb'
+$url64 = 'https://github.com/yt-dlp/yt-dlp/releases/download/2024.10.22/yt-dlp.exe'
+$checksum64 = '427f257578b99227e5364a0712bee649cab849be43e4916fe23a136305fd0151'
 $checksumType64 = 'sha256'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
