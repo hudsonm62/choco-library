dc6f55ea786189f624e0015b96b76d6bf5db7700
hudsonm62 <hudsonm62@users.noreply.github.com>
2024-10-26 15:32:07 +0000

    Choco-AU : Applied Automatic Package Updates

--- packages/lime3ds/tools/chocolateyInstall.ps1
+++ packages/lime3ds/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64 = ''
-$checksum64 = ''
+$url64 = 'https://github.com/Lime3DS/Lime3DS/releases/download/2118.2/lime3ds-2118.2-windows-msys2-installer.exe'
+$checksum64 = '1e1ae8cf66d6502c3eb20fbf28d0fb477c9aa1aa3fe7b4e37e46bb79d33e7d12'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
