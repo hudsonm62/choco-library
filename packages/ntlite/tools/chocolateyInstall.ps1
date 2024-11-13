dea32aff4accedb2426024f5c86fbe22ca220315
hudsonm62 <hudsonm62@users.noreply.github.com>
2024-11-13 04:09:57 +0000

    Choco-AU : Applied Automatic Package Updates

--- packages/ntlite/tools/chocolateyInstall.ps1
+++ packages/ntlite/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 $packageName = 'ntlite'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://downloads.ntlite.com/files/NTLite_setup_x86.exe'
-$checksum = 'fc523470f6a1b787a4e660bd64aeb4f95ab63095a2453b201403a446b28e3e90'
+$checksum = 'cf622ae575c40a28b0affd049c965c0fb9b1b577f952612c105fab84efc77bd7'
 $checksumType = 'sha256'
 $url64 = 'https://downloads.ntlite.com/files/NTLite_setup_x64.exe'
-$checksum64 = 'e5c66536bedec69c1ba6a28fd54b02819af417048275f03be759b1b2e850a0de'
+$checksum64 = 'a6126e3d4cec3565a8e1b70f67a0a277c2902b5de784ce22722943bdb32a19ad'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
