eaed79042f1c23fd4016b840c6b2b13ebba73a1f
Hudson <hudson@ziroau.com>
2024-11-24 01:38:37 +1100

    add github token to windirstat requests

--- packages/windirstat/update.ps1
+++ packages/windirstat/update.ps1
@@ -3,14 +3,14 @@ Import-Module Chocolatey-AU
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"       = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
             "(?i)(^\s*url64bit\s*=\s*)('.*')"       = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
-        "windirstat.nuspec"                 = @{
+        "windirstat.nuspec"             = @{
             "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
         }
     }
@@ -20,6 +20,9 @@ function global:au_GetLatest {
     $headers = @{
         "Accept"               = "application/vnd.github+json"
         "X-GitHub-Api-Version" = "2022-11-28"
+    }
+    if ($Token) {
+        $headers.Add("Authorization", "Bearer $($Token)")
     }
     $url = "https://api.github.com/repos/windirstat/windirstat/releases/latest"
     $latestRelease = Invoke-RestMethod -Uri $url -Headers $headers -Method Get
