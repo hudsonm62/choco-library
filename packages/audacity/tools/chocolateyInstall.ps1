acfe0bfaaf017e359c24e01e5397fad286a81665
hudsonm62 <hudsonm62@users.noreply.github.com>
2024-12-13 04:10:30 +0000

    Choco-AU : Applied Automatic Package Updates

--- packages/audacity/tools/chocolateyInstall.ps1
+++ packages/audacity/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url32 = 'https://github.com/audacity/audacity/releases/download/Audacity-3.7.0/audacity-win-3.7.0-32bit.exe'
-$checksum32 = '84c2643dae27a26703e725f97258ba8b8d428478fa10f0df141cf093aa6f2432'
-$url64 = 'https://github.com/audacity/audacity/releases/download/Audacity-3.7.0/audacity-win-3.7.0-64bit.exe'
-$checksum64 = '1034c8189843c4a2f284746369ce0ec1a3667b7b10d88aca2f44b68369c2347f'
+$url32 = 'https://github.com/audacity/audacity/releases/download/Audacity-3.7.1/audacity-win-3.7.1-32bit.exe'
+$checksum32 = '7842ef996492d13eb5b3095f6e7ae976d180494e9001a6642d61f9de1ebeed49'
+$url64 = 'https://github.com/audacity/audacity/releases/download/Audacity-3.7.1/audacity-win-3.7.1-64bit.exe'
+$checksum64 = '5eae3e6167be049f0995d86f24273e94f763c5a2b860ddb876d753ec8a69527e'
 
 $packageArgs = @{
   packageName    = "audacity"
