ed6755a66544263fb6e5c147cd34502c9462d041
hudsonm62 <hudsonm62@users.noreply.github.com>
2025-01-07 04:09:50 +0000

    Choco-AU : Applied Automatic Package Updates

--- packages/drawio/tools/chocolateyinstall.ps1
+++ packages/drawio/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'drawio'
   fileType       = 'MSI'
-  url64bit       = 'https://github.com/jgraph/drawio-desktop/releases/download/v26.0.3/draw.io-26.0.3.msi'
-  checksum64     = '13ff5a69b00a8d2bafe2c58a9d79e36022acacc823cbbb2ddbee432e3f55b98f'
+  url64bit       = 'https://github.com/jgraph/drawio-desktop/releases/download/v26.0.4/draw.io-26.0.4.msi'
+  checksum64     = '0be8230a99dd3f3751762421de425774e4fd44f7c1f4ebd40f04287c4a5a6d00'
   checksumType64 = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
